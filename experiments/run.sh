ff9ef483dae94e31ccd0d30806fa41a3823d427c
jiachenwestlake <48620507+jiachenwestlake@users.noreply.github.com>
2024-06-14 15:37:02 +0800

    Add files via upload

---
+++ experiments/run.sh
@@ -0,0 +1,7 @@
+python cli.py --config_yaml classification_manual_prompt_book.yaml \
+
+python cli.py --config_yaml classification_manual_prompt_dvd.yaml \
+
+python cli.py --config_yaml classification_manual_prompt_ele.yaml \
+
+python cli.py --config_yaml classification_manual_prompt_kit.yaml \
